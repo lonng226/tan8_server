89eec3dc0fcd1c4ef6723d51a6216f204c77b193
guoliawa <wanggl5@lenovo.com>
2016-06-02 21:01:34 +0800

    Add editalbum and update upload page.

--- upload.php
+++ upload.php
@@ -12,5 +12,27 @@ if (! empty ( $_FILES )) {
     mkdir ( $targetPath );
     $targetFile = $targetPath . $_FILES ['file'] ['name']; // 5
     move_uploaded_file ( $tempFile, $targetFile ); // 6
+    write_file_to_db($path, $name, $_FILES ['file'] ['name']);
 }
+
+/**
+ * Write the data to database
+ *
+ * @param str $type
+ *            type
+ *        str $albumname
+ *            albumname
+ *        str $filename
+ *            filename
+ */
+function write_file_to_db($type, $albumname, $filename) {
+    // Connect to database
+    $link = mysql_connect ( 'localhost', 'root', 'welcome1' ) or die ( 'Could not connect: ' . mysql_error () );
+    mysql_select_db ( 'tanqindb' ) or die ( 'Could not select database' );
+
+    $query = "insert into pre_album_tbl (type, albumname, filename) values ('$type', '$albumname', '$filename')";
+    $result = mysql_query ( $query ) or die ( 'Query failed: ' . mysql_error () );
+    mysql_close ( $link );
+}
+
 ?>
